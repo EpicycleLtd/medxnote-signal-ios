28cca1077ca64127a61d78576675338664899c4b
Jan Nemecek <jnemecek@razor.rs>
2017-02-23 20:06:43 +0100

    Add share extension target

---
+++ share/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  share
+//
+//  Created by Jan Nemecek on 2/22/17.
+//  Copyright © 2017 Open Whisper Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
