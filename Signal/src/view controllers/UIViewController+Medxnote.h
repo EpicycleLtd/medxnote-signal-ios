61e97db517e26de6329beb5bd637afc426ec5ebc
Jan Nemecek <jnemecek@razor.rs>
2017-07-14 12:27:31 +0200

    Add ForcePasscode flag to Info.plist

---
+++ Signal/src/view controllers/UIViewController+Medxnote.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Medxnote.h
+//  Medxnote
+//
+//  Created by Jan Nemecek on 14/7/17.
+//  Copyright © 2017 Open Whisper Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (Medxnote)
+
+@end
---
+++ Signal/src/view controllers/UIViewController+Medxnote.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Medxnote.m
+//  Medxnote
+//
+//  Created by Jan Nemecek on 14/7/17.
+//  Copyright © 2017 Open Whisper Systems. All rights reserved.
+//
+
+#import "UIViewController+Medxnote.h"
+
+@implementation UIViewController (Medxnote)
+
+@end
